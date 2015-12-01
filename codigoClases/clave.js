7854e8af68c6d34da724c126ee56a52fb88ecc79
hanmiton <hanmilton_12@outlook.com>
2015-12-01 17:25:59 -0500

    empezando con palabras claves UX

---
+++ codigoClases/clave.js
@@ -0,0 +1,20 @@
+var claves = {
+	"getElementsByTagName":"acceder a elementos del DOM por tag";
+	"getElementById":"elementos por id",
+	"createElement": "crear elemento del DOM",
+	"createTextNode": "crear noddo de texto",
+	"appendChild": "añadir un hijo",
+	"setAttribute": "añadir atributos de css3",
+	"parentNode": "coje el padre",
+	"insertBefore":"(elemento,referencia) agregar antes",
+	"innerHTML": "cojer codigo html dentro de una etiqueta",
+	"textContent":"solo genera codiog no html5 text dentro etiqueta",
+	"replaceChild":"(elemento,remplazo) reemplazr nodos DOM",
+	"removeChild":"borrar elemento del DOM",
+	".":"accedemos atributos dentro de elementos",
+	
+
+
+
+
+};
